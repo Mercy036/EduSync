c8054b71850a923443cde31a6aac516403fe960d
Ansh <144668849+Mercy036@users.noreply.github.com>
2025-12-26 21:20:22 +0530

    css fixing

--- src/app/notes/layout.tsx
+++ src/app/notes/layout.tsx
@@ -1,4 +1,4 @@
-import "./dashboardLayout.css";
+import "./notesLayout.css";
 import SideBar from "@/src/components/SideBar";
 
 export default function DashboardLayout({
@@ -7,9 +7,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="dashboard-layout">
+    <div className="notes-layout">
       <SideBar />
-      <main className="dashboard-main">
+      <main className="notes-main">
         {children}
       </main>
     </div>
